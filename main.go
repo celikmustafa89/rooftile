6dec2a29a624229dbeca057d802842c7dcdc57eb
Mustafa Celik <mustafacelik@Mustafas-MacBook-Pro-2.local>
2018-07-15 01:33:05 +0300

    port 8080 -> 8000

--- main.go
+++ main.go
@@ -95,6 +95,6 @@ func main() {
 	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
 	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
 
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":8000", r))
 
 }
